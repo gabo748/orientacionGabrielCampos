3e6e2685f1c1234deb540525f7f01d5dd8c23624
unknown <UFG@CC6-06.ufg.local>
2024-04-23 20:08:08 -0600

    not finished

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")

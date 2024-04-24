8011b78ae5bee3a79e0238321ed5c70ce6558b06
unknown <UFG@CC6-06.ufg.local>
2024-04-23 19:20:34 -0600

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ufg.edu.fis.amb.orientacionGabrielCampos"
+include(":app")
